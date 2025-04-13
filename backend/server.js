17ec365bf74f8265977d0219c21d019d881d82f7
Abhinav Sharma <abhisharma19091999@gmail.com>
2025-04-13 12:05:11 +0530

    some improvement

--- backend/server.js
+++ backend/server.js
@@ -18,17 +18,7 @@ const allowedOrigins = [
     "http://localhost:5173"             
   ];
   
-  app.use(cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true 
-  }));
+  app.use(cors({origin:process.env.Origin, credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 
