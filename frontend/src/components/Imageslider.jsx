233e145b0c0f13a6a550d43c364eb1835c2f415e
Abhinav Sharma <abhisharma19091999@gmail.com>
2025-05-19 16:39:24 +0530

    NEw

--- frontend/src/components/Imageslider.jsx
+++ frontend/src/components/Imageslider.jsx
@@ -9,8 +9,7 @@ import "swiper/css/pagination";
 const images = [
   "https://www.globalialogisticsnetwork.com/blog/wp-content/uploads/2021/09/tracking-1.jpg",
   "https://jklogisticsgroup.com/wp-content/uploads/2023/09/Customs-Broker.png",
-  "https://static.wixstatic.com/media/e1d346_7807378aa5554c88a1e581d59d372dec~mv2.png/v1/fill/w_1000,h_563,al_c,q_90,usm_0.66_1.00_0.01/e1d346_7807378aa5554c88a1e581d59d372dec~mv2.png",
-  "https://www.shippingaviation.com/image/custom-clearing-agent.jpg"
+  "https://static.wixstatic.com/media/e1d346_7807378aa5554c88a1e581d59d372dec~mv2.png/v1/fill/w_1000,h_563,al_c,q_90,usm_0.66_1.00_0.01/e1d346_7807378aa5554c88a1e581d59d372dec~mv2.png"
 ];
 
 function ImageSlider() {
